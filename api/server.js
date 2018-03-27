480027ca1b0df4f1a025906612d8a01e123e22e3
Tom Wilson <tom@jackhq.com>
2018-03-27 13:51:51 -0400

    secure api

--- api/server.js
+++ api/server.js
@@ -1,6 +1,9 @@
 require('dotenv').config()
 
 const express = require('express')
+const jwt = require('express-jwt')
+const jwks = require('jwks-rsa')
+
 const bodyParser = require('body-parser')
 const app = express()
 const cors = require('cors')
@@ -10,6 +13,20 @@ const db = PouchDB('quotes3')
 const randomInt = require('random-int')
 
 app.use(cors())
+
+app.use(
+  jwt({
+    secret: jwks.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: 'https://quotable.auth0.com/.well-known/jwks.json'
+    }),
+    audience: 'https://quote-api.jrscode2.xyz',
+    issuer: 'https://quotable.auth0.com/',
+    algorithms: ['RS256']
+  })
+)
 app.get('/quote', async (req, res) => {
   // pick a random number between 1 and 75,000
   res.send(await db.get(randomInt(75966).toString()))
